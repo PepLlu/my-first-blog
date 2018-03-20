56b05bb27c67d9ec2c2c4cf167b84d41aa78c869
PepLlu <peplluissm@gmail.com>
2018-03-20 10:06:10 +0000

    all

---
+++ my-first-blog/pepllu/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pepllu/my-first-blog/my-first-blog/pepllu/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- pepllu/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jsimon/workspace/djangogirls/pepllu/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
