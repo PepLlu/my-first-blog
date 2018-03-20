8c6f8c7e917a0b08875a58fe6143610b424adb00
PepLlu <peplluissm@gmail.com>
2018-03-20 11:02:26 +0000

    posts

--- my-first-blog/blog/views.py
+++ my-first-blog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 def post_list(request):
-        return render(request, 'blog/post_list.html', {})
+	return render(request, 'blog/post_list.html', {'posts': posts})
+posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
