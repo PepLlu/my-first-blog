ee24d1691636ad3e09ad035a3ee8f8e1eec7da30
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-20 14:49:47 +0100

    tot

--- my-first-blog/mysite/urls.py
+++ my-first-blog/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'', include('blog.urls')),
+	url(r'^$', views.post_list),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
    ]
