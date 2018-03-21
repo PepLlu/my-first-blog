4594d9c98e8072bff1cb5c29b1008dc24286b328
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-21 08:11:32 +0100

    config

--- my-first-blog/blog/views.py
+++ my-first-blog/blog/views.py
@@ -17,13 +17,13 @@ def post_new(request):
 
 def post_edit(request, pk):
         post = get_object_or_404(Post, pk=pk)
-	if request.method == "POST":
+if request.method == "POST":
 	form = PostForm(request.POST, instance=post)
-	if form.is_valid():
+if form.is_valid():
 	post = form.save(commit=False)
 	post.author = request.user
 	post.save()
 	return redirect('post_detail', pk=post.pk)
-	else:
+else:
 	form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
