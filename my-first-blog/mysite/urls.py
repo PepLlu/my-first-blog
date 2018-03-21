30833686f703852936c062bb8365192209600719
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-21 09:42:23 +0100

    finish

--- my-first-blog/mysite/urls.py
+++ my-first-blog/mysite/urls.py
@@ -4,6 +4,7 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^admin/', include)admin.site.url)),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
