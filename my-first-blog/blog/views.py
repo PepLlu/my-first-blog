8cd9a7b6839044771b0eb4bf733b6f4e906258ef
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-21 08:09:44 +0100

    config

--- my-first-blog/blog/views.py
+++ my-first-blog/blog/views.py
@@ -9,21 +9,21 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    posts= Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return render(request, 'blog/post_list.html', {'posts': posts})
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-if request.method == "POST":
-        form = PostForm(request.POST)
-    else:
         form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
 
-if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-
-return redirect('post_detail', pk=post.pk)
+def post_edit(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+	form = PostForm(request.POST, instance=post)
+	if form.is_valid():
+	post = form.save(commit=False)
+	post.author = request.user
+	post.save()
+	return redirect('post_detail', pk=post.pk)
+	else:
+	form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
