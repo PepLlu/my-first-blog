40ef3cb675b63c8ff2f7cd0604e0c4e6b93016fa
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-21 09:58:53 +0100

    fin

--- my-first-blog/blog/views.py
+++ my-first-blog/blog/views.py
@@ -37,4 +37,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
         else:
             form = PostForm(instance=post)
-        return render(request, 'blog/post_edit.html', {'form': form})
+            return render(request, 'blog/post_edit.html', {'form': form})
