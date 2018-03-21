6745fabbd7f5ebe71bbb9b53711e561b9571d3ae
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-03-21 09:50:21 +0100

    finish

--- my-first-blog/mysite/urls.py
+++ my-first-blog/mysite/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from blog import views
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.post_list),
-    url(r'^admin/', include)admin.site.urls)),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
